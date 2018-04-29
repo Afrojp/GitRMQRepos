f071c862f5445b7414042330c12e9993e0bae7f5
Afrojp <afrojkhanpathan@gmail.com>
2018-04-29 12:14:06 -0400

    Consumer added

---
+++ Consumer/MainRMQConsumerProg/Consumer.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainRMQConsumerProg
+{
+    class Consumer
+    {
+        public void comsumeMessage()
+        {
+            var factory = new ConnectionFactory();
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.HostName = "localhost";
+
+            var conn = factory.CreateConnection();
+            var channel = conn.CreateModel();
+
+            //channel.QueueDeclare(queue:"Test-Q",durable:false,exclusive:false,autoDelete:false,arguments:null);
+            channel.QueueDeclare("Test_Q", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+             {
+                 var body = ea.Body;
+                 var message = Encoding.UTF8.GetString(body);
+                 Console.WriteLine("Consumed message -" + message);
+             };
+
+            channel.BasicConsume("Test_Q", true, consumer);
+            Console.ReadLine();
+       
+        }
+    }
+}
---
+++ Consumer/MainRMQConsumerProg/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainRMQConsumerProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consumer c = new Consumer();
+            c.comsumeMessage();
+            Console.ReadLine();
+        }
+    }
+}
